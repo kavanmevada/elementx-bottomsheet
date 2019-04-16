c78e9b45cb894e91db8e7167b603d1086d17ac52
Kavan Mevada <kavanmevada@Kavans-iMac.local>
2019-04-16 20:48:46 +0530

    Copyright update

--- bottomsheet/src/main/java/elementx/bottomsheet/BottomSheetLayout.kt
+++ bottomsheet/src/main/java/elementx/bottomsheet/BottomSheetLayout.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2019  Kavan Mevada
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package elementx.bottomsheet
 
 import android.animation.ValueAnimator
@@ -10,9 +27,6 @@ import android.widget.LinearLayout
 import elementx.ui.bottomsheet.R
 
 
-/**
- * Created by quentin on 07/11/2017.
- */
 class BottomSheetLayout : LinearLayout {
     constructor(context: Context) : super(context) {
         initView(null)
